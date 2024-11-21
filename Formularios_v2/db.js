ad6910dd6cf9ce38d9280d8a17f46f4947b3381f
Zekken2k <pslopez91@soy.sena.edu.co>
2024-11-21 13:21:05 -0500

    Puertos y variables de entorno

--- Formularios_v2/db.js
+++ Formularios_v2/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();  // Cargar las variables de entorno desde el archivo .env
 
+// Crear la conexión usando las variables de entorno
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '51919616Nexus',
-    database: 'EpaArepaRellena'
+    host: process.env.DB_HOST,        // Usar la variable de entorno DB_HOST
+    user: process.env.DB_USER,        // Usar la variable de entorno DB_USER
+    password: process.env.DB_PASSWORD, // Usar la variable de entorno DB_PASSWORD
+    database: process.env.DB_DATABASE // Usar la variable de entorno DB_DATABASE
 });
 
+// Conectar a la base de datos
 connection.connect((err) => {
     if (err) {
         console.error('Error conectando a la base de datos:', err);
